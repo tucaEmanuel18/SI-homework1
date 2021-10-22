c70f429a4bc8f6003b805666fab8b8df1ff7fcc1
tucaEmanuel18 <emanuel.tuca@info.uaic.ro>
2021-10-22 10:08:53 +0300

    debugging ECB cipher

--- project/main.py
+++ project/main.py
@@ -1,4 +1,5 @@
 import enum
+import binascii
 from Crypto.Cipher import AES
 import os
 
@@ -24,7 +25,7 @@ def get_blocks(message, size):
     bits = to_bits(message)
     bit_blocks = []
     # fill with 0 if is necessary
-    rest = len(bits) % size
+    rest = size - len(bits) % size
     for i in range(rest):
         bits.append(0)
 
@@ -45,31 +46,42 @@ def xor(str1, str2):
 class Cipher:
     def __init__(self, key):
         self.key = key
-        self.cipher = AES(key)
+        self.cipher = AES.new(key, AES.MODE_ECB)
 
 
 class EcbCipher(Cipher):
     def __init__(self, key):
-        Cipher.__init__(key)
+        super().__init__(key)
 
     def encrypt(self, message):
-        blocks = get_blocks(message, 128)
+        size = 16
         cipher_text = ""
-        for block in blocks:
-            cipher_text += self.cipher.encrypt(block)
+        # fill with 0
+        if len(message) % size != 0:
+            message = message + (size - len(message) % size) * "0"
+
+        for i in range(len(message) // size):
+            start_pos = i * size
+            block = message[start_pos:start_pos + size]
+            encrypted_block = self.cipher.encrypt(block.encode('utf-8'))
+
+            cipher_text = cipher_text + binascii.hexlify(encrypted_block).decode('utf-8')
         return cipher_text
 
     def decrypt(self, encrypted_message):
-        blocks = get_blocks(encrypted_message, 128)
+        size = 32
         plain_text = ""
-        for block in blocks:
-            plain_text += self.cipher.decrypt(block)
+        for i in range(len(encrypted_message) // size):
+            start_pos = i * size
+            block = encrypted_message[start_pos:start_pos + size]
+            plain_text = plain_text + self.cipher.decrypt(binascii.unhexlify(block.encode('utf-8'))).decode('utf-8')
+
         return plain_text
 
 
 class OfbCipher(Cipher):
     def __init__(self, key):
-        Cipher.__init__(key)
+        super().__init__(key)
         self.vector = None
 
     def vector_init(self):
@@ -80,7 +92,7 @@ class OfbCipher(Cipher):
         blocks = get_blocks(message, 128)
         cipher_text = ""
         for block in blocks:
-            self.vector = self.cipher.encrypt(self.vector)
+            self.vector = self.cipher.encrypt(self.vector.encode(encoding='UTF-8'))
             cipher_text += xor(block, self.vector)
         return cipher_text
 
@@ -89,7 +101,7 @@ class OfbCipher(Cipher):
         blocks = get_blocks(encrypted_message, 128)
         plain_text = ""
         for block in blocks:
-            self.vector = self.cipher.decrypt(self.vector)
+            self.vector = self.cipher.decrypt(self.vector.encode(encoding='UTF-8'))
             plain_text += xor(block, self.vector)
         return plain_text
 
@@ -97,6 +109,7 @@ class OfbCipher(Cipher):
 class OperationalMode(enum.Enum):
     ECB = 1
     OFB = 2
+
 
 class CommunicationNode:
     def __init__(self):
@@ -106,7 +119,7 @@ class CommunicationNode:
 
     def start_communication(self, another_node: "CommunicationNode", mode: "OperationalMode"):
         self.send_operational_mode(another_node, mode)
-        encrypted_key = self.ask_for_a_key()
+        encrypted_key = key_manager.get_random_key(self.mode)
         self.receive_encrypted_key(encrypted_key)
         self.send_encrypted_key(encrypted_key, another_node)
 
@@ -121,12 +134,9 @@ class CommunicationNode:
         another_node.receive_encrypted_key(encrypted_key)
 
     def receive_encrypted_key(self, encrypted_key):
-        public_key = KeyManager.get_public_key()
-        cipher = AES(public_key)
+        public_key = key_manager.get_public_key()
+        cipher = AES.new(public_key, AES.MODE_ECB)
         self.private_key = cipher.decrypt(encrypted_key)
-
-    def ask_for_a_key(self):
-        return KeyManager.get_random_key(self.mode)
 
     def send_message(self, message, another_node: "CommunicationNode"):
         encrypted_message = self.encrypt_message(message)
@@ -137,30 +147,41 @@ class CommunicationNode:
             cipher = EcbCipher(self.private_key)
         else:
             cipher = OfbCipher(self.private_key)
-
         return cipher.encrypt(message)
+
+    def decrypt_message(self, message):
+        if self.mode == OperationalMode.ECB:
+            cipher = EcbCipher(self.private_key)
+        else:
+            cipher = OfbCipher(self.private_key)
+        return cipher.decrypt(message)
 
     def print_messages(self):
         for message in self.memory:
-            print(message)
+            print(self.decrypt_message(message))
+
 
 class KeyManager:
     def __init__(self):
-        self.public_key = "testamcriptarea"
+        self.public_key = "testamcriptareab"
+        self.public_key_bytes = self.public_key.encode(encoding='UTF-8')
 
-    def get_random_key(self, mode: "Operational_Mode"):
+    def get_random_key(self, mode):
+        if mode == OperationalMode.ECB:
+            mode_parameter = AES.MODE_ECB
+        else:
+            mode_parameter = AES.MODE_OFB
         new_key = os.urandom(16)
-        cipher = AES.new(self.public_key)
+        cipher = AES.new(self.public_key_bytes, mode_parameter)
         return cipher.encrypt(new_key)
 
     def get_public_key(self):
-        return self.public_key
+        return self.public_key_bytes
 
 
 def read_from_file(file_name):
     file = open(file_name, "r")
     return file.read()
-
 
 if __name__ == '__main__':
     file_name = "input.txt"
@@ -171,6 +192,8 @@ if __name__ == '__main__':
     B = CommunicationNode()
     A.start_communication(B, operational_mode)
     B.send_message("ack", A)
+    print("A:")
+    A.print_messages()
     A.send_message(read_from_file(file_name), B)
+    print("B:")
     B.print_messages()
-
